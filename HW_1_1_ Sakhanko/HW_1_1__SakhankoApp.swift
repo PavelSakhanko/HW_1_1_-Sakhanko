5ad15ada1b71a56600c1d60f8a799db63c0fab1d
Pavel Sakhanko <iospaveldev@gmail.com>
2020-12-01 17:33:37 +0300

    Initial Commit

---
+++ HW_1_1_ Sakhanko/HW_1_1__SakhankoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_1_1__SakhankoApp.swift
+//  HW_1_1_ Sakhanko
+//
+//  Created by Pavel Sakhanko on 1.12.20.
+//
+
+import SwiftUI
+
+@main
+struct HW_1_1__SakhankoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
